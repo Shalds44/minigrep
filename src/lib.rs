76f5173567a0acfb906b9bcf15eb9165d5501226
Ronan <ronan@OrdinatedeRonan.lan>
2024-05-05 14:13:13 +0200

    add iterateur

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,52 @@
+extern crate dotenv;
+
+use std::env::var;
 use std::error::Error;
 use std::fs;
+
+use dotenv::dotenv;
+use std::env;
 pub struct Config {
     pub query: String,
-    pub file_path: String
+    pub file_path: String,
+    pub sensible_casse: bool
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args : env::Args) -> Result<Config, &'static str> {
+        dotenv().ok();
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Nous n'avons pas de chaîne de caractère"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Nous n'avons pas de nom de fichier"),
+        };
+
+        let sensible_casse = env::var("MINIGREP_INSENSIBLE_CASSE").is_err();
+
+        Ok(Config { 
+            query, 
+            file_path, 
+            sensible_casse 
+        })
     }
 }
 
 pub fn run(config: Config)->Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.file_path)?;
+
+    let resultats = if config.sensible_casse {
+        search(&config.query, &contents)
+    }else{
+        rechercher_insensible_casse(&config.query, &contents)
+    };
     
-    for line in search(&config.query, &contents){
+    for line in resultats{
         println!("{line}");
     }
     Ok(())
@@ -32,24 +57,47 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn sensible_casse() {
         let query ="duct";
         let contents = "\
         Rust:
-        safe, fast, productive.
-        Pick three.";
+        sécurité, rapidité, productivité.
+        Obtenez les trois en même temps.
+        Duck tape.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(vec!["sécurité, rapidité, productivité."], search(query, contents));
+    }
+
+    #[test]
+    fn insensible_casse(){
+        let query ="rUsT";
+        let contents = "\
+        Rust:
+        sécurité, rapidité, productivité.
+        Obtenez les trois en même temps.
+        C'est pas rustique.";
+
+        assert_eq!(vec!["Rust:", "C'est pas rustique."], 
+        rechercher_insensible_casse(query, contents));
     }
 }
 
 pub fn search<'a>(query: &str, contents:&'a str)->Vec<&'a str>{
 
-    let mut results = Vec::new();
+    contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
+}
+
+pub fn rechercher_insensible_casse<'a>(query: &str, contents: &'a str)->Vec<&'a str>{
+    let mut result = Vec::new();
     for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line.trim())
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            result.push(line.trim());
         }
     }
-    results
+
+    result
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,13 @@ use minigrep::{Config, run};
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     // dbg!(config);
     if let Err(e) = run(config){
-        println!("Application Error {e}");
+        eprintln!("Application Error {e}");
         process::exit(1);
     }
 
